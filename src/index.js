0d87bf7cd72c258fcaaf2976ee038d4845a9ef5b
Ariel Spencer <arielspencer.tech@gmail.com>
2024-08-29 22:10:16 -0300

    feat: convert to lowercase, split, and check words per paragraph

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (err, data) => {
-  verificaPalavrasDuplicadas(data);
+  quebraEmParagrafos(data);
 })
+
+function quebraEmParagrafos(data) {
+  const paragrafos = data.toLowerCase().split('\n');
+  const contagem = paragrafos.map((paragrafo) => {
+    return verificaPalavrasDuplicadas(paragrafo);
+  })
+  console.log(contagem);
+}
 
 function verificaPalavrasDuplicadas(data) {
   const listaPalavras = data.split(' ');
@@ -13,5 +21,5 @@ function verificaPalavrasDuplicadas(data) {
   listaPalavras.forEach(palavra => {
     resultado[palavra] = (resultado[palavra] || 0) + 1
   })
-  console.log(resultado);
+  return resultado;
 }
