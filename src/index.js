4b8ba1fe172c17bdf7df8ccc4e21888b896e79c8
Ariel Spencer <arielspencer.tech@gmail.com>
2024-08-29 21:37:27 -0300

    feat: add function to detect and count duplicate words in file content

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (err, data) => {
-  console.log(data);
+  verificaPalavrasDuplicadas(data);
 })
+
+function verificaPalavrasDuplicadas(data) {
+  const listaPalavras = data.split(' ');
+  const resultado = {};
+  listaPalavras.forEach(palavra => {
+    resultado[palavra] = (resultado[palavra] || 0) + 1
+  })
+  console.log(resultado);
+}
