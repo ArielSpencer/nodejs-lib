6cd58da6091fca2188f1a29eb3e046f4c715d134
Ariel Spencer <arielspencer.tech@gmail.com>
2024-08-29 21:20:15 -0300

    feat: add file reading functionality to log content from provided path

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+const fs = require('fs'); // lib/fs.js
+
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
-console.log(link);
+fs.readFile(link, 'utf-8', (err, data) => {
+  console.log(data);
+})
