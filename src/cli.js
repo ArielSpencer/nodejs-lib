79344c0fe1b57e2e37e504972cb80528bad49c76
Ariel Spencer <arielspencer.tech@gmail.com>
2024-09-02 19:50:00 -0300

    feat: add file operations and results folder

--- src/cli.js
+++ src/cli.js
@@ -4,12 +4,25 @@ import { contaPalavras } from './index.js';
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
+const endereco = caminhoArquivo[3];
 
 fs.readFile(link, 'utf-8', (err, data) => {
   try {
     if (err) throw err
-    contaPalavras(data);
+    const resultado = contaPalavras(data);
+    criaESalvaArquivo(resultado, endereco);
   } catch (erro) {
     trataErros(erro)
   }
 })
+
+async function criaESalvaArquivo(listaPalavras, endereco) {
+  const arquivoNovo = `${endereco}/resultado.txt`;
+  const textoPalavras = JSON.stringify(listaPalavras);
+  try {
+    await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    console.log('Arquivo criado')
+  } catch (erro) {
+    throw erro;
+  }
+}
